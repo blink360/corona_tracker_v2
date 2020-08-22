5bbaab6799f6deeb8be0e0fa874efb553f5e2b9f
Pranish Rajbhandari <rajbhandari.pranish@gmail.com>
2020-08-22 18:01:15 +0545

    Updates

---
+++ precache-manifest.805acd9ff6f23558d5ca7a748f630999.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b9526b7d4aeeecaea7b4d41f88f2f1",
+    "url": "/corona_tracker_v2/index.html"
+  },
+  {
+    "revision": "0bf779bef04ce7344171",
+    "url": "/corona_tracker_v2/static/css/main.63386a80.chunk.css"
+  },
+  {
+    "revision": "ea73402d2aeb90661d8f",
+    "url": "/corona_tracker_v2/static/js/2.79c9569d.chunk.js"
+  },
+  {
+    "revision": "dc8645a911d3a18415a520f4582ea28f",
+    "url": "/corona_tracker_v2/static/js/2.79c9569d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bf779bef04ce7344171",
+    "url": "/corona_tracker_v2/static/js/main.f6c74f09.chunk.js"
+  },
+  {
+    "revision": "1c65d4e3c7e64d093b87",
+    "url": "/corona_tracker_v2/static/js/runtime-main.66b097cb.js"
+  },
+  {
+    "revision": "8e4628f7b38d5a25988c85bf08a42f76",
+    "url": "/corona_tracker_v2/static/media/logo.8e4628f7.svg"
+  }
+]);
